d2cc509142645ec69263f99468ab540d1ed7ed76
dnowak <dorota.iza.nowak@gmail.com>
2019-12-07 17:24:57 +0100

    horizontal menu created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React, {Component} from "react";
 import Menu from "./Menu";
 import {Parallax} from "react-parallax";
-import React, {Component} from "react";
+//import './css/main.css';
 
 const styles = {
     fontFamily: "sans-serif",
@@ -19,60 +20,66 @@ const insideStyles = {
 class App extends Component {
     render() {
         return (
-            <div style={styles}>
-                <Menu name="Parallax"/>
-                <Parallax bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide1.jpg')}
-                          strength={500}>
-                    <div style={{height: 500}}>
-                        <div style={insideStyles}>WELCOM TO CUTIE PIE!</div>
+            <div>
+                <header className="entry-header">
+                    <div className="inner">
+                        <Menu name="Parallax"/>
                     </div>
-                </Parallax>
-                <h1>| | |</h1>
-                <Parallax bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide3.jpg')}
-                          blur={{min: -1, max: 2}}>
-                    <div style={{height: 500}}>
-                        <div style={insideStyles}>The best bakery in the world!</div>
-                    </div>
-                </Parallax>
-                <h1>| | |</h1>
-                <Parallax bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide4.jpg')}
-                          strength={400}>
-                    <div style={{
-                        height: 500
-                    }}>
-                        <div style={insideStyles}>Reverse direction</div>
-                    </div>
-                </Parallax>
-                <h1>| | |</h1>
-                <Parallax
-                    bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide5.jpg')}
-                    strength={300}
-                    renderLayer={percentage => (
-                        <div>
-                            <div
-                                style={{
-                                    position: "absolute",
-                                    background: `rgba(255, 255, 255, ${percentage * 1})`,
-                                    left: "50%",
-                                    top: "50%",
-                                    borderRadius: "50%",
-                                    transform: "translate(-50%,-50%)",
-                                    width: percentage * 500,
-                                    height: percentage * 500
-                                }}
-                            />
+                </header>
+                <div style={styles}>
+                    <Parallax bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide1.jpg')}
+                              strength={500}>
+                        <div style={{height: 500}}>
+                            <div style={insideStyles}>WELCOM TO CUTIE PIE!</div>
                         </div>
-                    )}
-                >
-                    <div style={{height: 500}}>
-                        <div style={insideStyles}>renderProp</div>
-                    </div>
-                </Parallax>
-                <Parallax>
-                    <div style={{height: 500}}>
-                        <div style={insideStyles}>More questions? Contact us!</div>
-                    </div>
-                </Parallax>
+                    </Parallax>
+                    <h1>| | |</h1>
+                    <Parallax bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide3.jpg')}
+                              blur={{min: -1, max: 2}}>
+                        <div style={{height: 500}}>
+                            <div style={insideStyles}>The best bakery in the world!</div>
+                        </div>
+                    </Parallax>
+                    <h1>| | |</h1>
+                    <Parallax bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide4.jpg')}
+                              strength={400}>
+                        <div style={{
+                            height: 500
+                        }}>
+                            <div style={insideStyles}>Reverse direction</div>
+                        </div>
+                    </Parallax>
+                    <h1>| | |</h1>
+                    <Parallax
+                        bgImage={require('C:\\Users\\Dorota Nowak\\Desktop\\PWWW\\myapp\\src\\img\\slide5.jpg')}
+                        strength={300}
+                        renderLayer={percentage => (
+                            <div>
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        background: `rgba(255, 255, 255, ${percentage * 1})`,
+                                        left: "50%",
+                                        top: "50%",
+                                        borderRadius: "50%",
+                                        transform: "translate(-50%,-50%)",
+                                        width: percentage * 500,
+                                        height: percentage * 500
+                                    }}
+                                />
+                            </div>
+                        )}
+                    >
+                        <div style={{height: 500}}>
+                            <div style={insideStyles}>renderProp</div>
+                        </div>
+                    </Parallax>
+                    <Parallax>
+                        <div style={{height: 500}}>
+                            <div style={insideStyles}>More questions? Contact us!</div>
+                        </div>
+                    </Parallax>
+                </div>
             </div>
         )
     }
--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import {
     Route,
     NavLink,
@@ -12,6 +13,30 @@ class Menu extends Component {
     }
     render() {
         return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>);
+/*
             <HashRouter>
                 <div>
                     <h1>MENU</h1>
@@ -25,7 +50,7 @@ class Menu extends Component {
                         <Route exact path="/order" component={Order}/>
                     </div>
                 </div>
-            </HashRouter>);
+            </HashRouter>);*/
     }
 }
 
